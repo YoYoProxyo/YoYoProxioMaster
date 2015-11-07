76efec98152869693f75fde079a40143334ddc03
Tom Ridd <twridd@gmail.com>
2015-11-07 11:46:29 +0000

    Initial Commit

---
+++ YoYoProxioMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YoYoProxioMaster
+//
+//  Created by Tom Ridd on 07/11/2015.
+//  Copyright © 2015 Tom Ridd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YoYoProxioMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoYoProxioMaster
+//
+//  Created by Tom Ridd on 07/11/2015.
+//  Copyright © 2015 Tom Ridd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
